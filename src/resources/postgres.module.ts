82f3a67ba37fdaeb9f12d0549ca439b3131dd447
Joan Lloret <juallom@gmail.com>
2023-02-01 17:11:57 +0100

    husky hook, dockerize app, connect postgres resource

---
+++ src/resources/postgres.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('POSTGRES_HOST'),
+        port: +configService.get('POSTGRES_PORT'),
+        username: configService.get('POSTGRES_USERNAME'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        database: configService.get('POSTGRES_NAME'),
+        entities: [],
+      }),
+    }),
+  ],
+})
+export class PostgresModule {}
